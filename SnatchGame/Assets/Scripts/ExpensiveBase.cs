12761a13edc6cc04db0be06f9386628f6d545cb3
Evy Avila <384036@colum.edu>
2023-10-04 23:47:27 -0500

    Implemented Object Values

---
+++ SnatchGame/Assets/Scripts/ExpensiveBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ExpensiveBase : MonoBehaviour
+{
+    public string Name { get; set; }
+    public double Value { get; set; }
+}
--- SnatchGame/Assets/Scripts/ExpensiveObject.cs
+++ SnatchGame/Assets/Scripts/ExpensiveObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExpensiveObject : MonoBehaviour
+public class ExpensiveObject : ExpensiveBase
 {
     private Material objColorNormal;
 
@@ -11,17 +11,28 @@ public class ExpensiveObject : MonoBehaviour
 
     private Renderer rend;
 
+    [SerializeField]
+    private string objName;
+    
+    //[SerializeField]
+    //private double objValue;
+
     // Start is called before the first frame update
     void Start()
     {
+        Name = objName;
+        Value = Random.Range(10, 150);
+
         objColorNormal = GetComponent<Renderer>().material;
         rend = GetComponent<MeshRenderer>();
+
+        Debug.Log(Name + " is worth: " + Value.ToString("c"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     private void OnTriggerEnter(Collider other)
